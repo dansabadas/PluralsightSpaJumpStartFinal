5737e560c8e711d1fba8a539dbc1e379ea34142c
AD001\z003p8vs <dan.sabadas@gmail.com>
2018-01-03 19:11:17 +0200

    done chapter 3

---
+++ SPAJumpStart/App/services/dump.js
@@ -0,0 +1,48 @@
+define(
+    ///usage: <div data-bind="dump: $data, enable: false"></div>
+    function () {
+        function toJSON(rootObject, replacer, spacer) {
+            var cache = [];
+            var plainJavaScriptObject = ko.toJS(rootObject);
+            var replacerFunction = replacer || cycleReplacer;
+
+            var output = ko.utils.stringifyJson(plainJavaScriptObject, replacerFunction, spacer || 2);
+            cache = null;
+            return output;
+
+            function cycleReplacer(key, value) {
+                if (['entityAspect', 'entityType', '_$typeName'].indexOf(key) !== -1) {
+                    return;
+                }
+                if (typeof value === 'object' && value !== null) {
+                    if (cache.indexOf(value) !== -1) {
+                        return;
+                    }
+                    cache.push(value);
+                }
+                return value;
+            }
+        }
+
+        ko.bindingHandlers.dump = {
+            init: function (element, valueAccessor, allBindingsAccesor, viewModel, bindingContext) {
+                var context = valueAccessor();
+                var allBindings = allBindingsAccesor;
+                var pre = document.createElement('pre');
+                element.appendChild(pre);
+
+                var dumpJSON = ko.computed({
+                    read: function () {
+                        var en = allBindings.enable === undefined || allBindings.enable;
+                        return en ? toJSON(context, null, 2) : '';  // peopleArr=> context
+                    },
+                    disposeWhenNodeIsRemoved: element
+                });
+
+                ko.applyBindingsToNode(pre,
+                    { text: dumpJSON, visible: dumpJSON });
+
+                return { controlsDescendentBindings: true };
+            }
+        }; 
+    });
